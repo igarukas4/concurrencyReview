12e36e1c795ce9a89cfb58d9f3c94bbbd4b1e37f
igarukas4 <igarukas4@gmail.com>
2024-08-05 21:24:22 +0800

    slight changes

--- 6/main.go
+++ 6/main.go
@@ -5,7 +5,7 @@ import (
 )
 
 func produceNumbers(evenCh, oddCh, errCh chan int) {
-	for i := 1; i <= 22; i++ {
+	for i := 1; i <= 25; i++ {
 		if i > 20 {
 			errCh <- i
 		} else if i%2 == 0 {
@@ -26,23 +26,27 @@ func main() {
 
 	go produceNumbers(evenCh, oddCh, errCh)
 
-	for {
+	evenChOpen, oddChOpen, errChOpen := true, true, true
+	for evenChOpen || oddChOpen || errChOpen {
 		select {
 		case num, ok := <-evenCh:
 			if ok {
 				fmt.Printf("Received an even number: %d\n", num)
+			} else {
+				evenChOpen = false
 			}
 		case num, ok := <-oddCh:
 			if ok {
 				fmt.Printf("Received an odd number: %d\n", num)
+			} else {
+				oddChOpen = false
 			}
 		case err, ok := <-errCh:
 			if ok {
 				fmt.Printf("Error: number %d is greater than 20\n", err)
+			} else {
+				errChOpen = false
 			}
-		}
-		if len(evenCh) == 0 && len(oddCh) == 0 && len(errCh) == 0 {
-			break
 		}
 	}
 }
